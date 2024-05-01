a37a10dcb0b4837ee517338351def002fd788fb7
Mauricio hernandez <maurihed@gmail.com>
2024-05-01 17:31:12 -0600

    Faq challenge solved

---
+++ faq-accordion-main/assets/scripts.js
@@ -0,0 +1,22 @@
+const __ = (s) => document.querySelectorAll(s);
+const ENTER_KEY = "Enter";
+const SPACE_KEY = " ";
+
+document.addEventListener("DOMContentLoaded", () => {
+    __(".faq-list-checkbox").forEach((checkbox) => {
+        checkbox.addEventListener("change", (e) => {
+            const content = e.currentTarget.parentNode.parentNode.querySelector(".faq-list-content");
+            if (content) {
+                content.classList.toggle("expanded");
+            }
+        })
+    });
+
+    __(".faq-list-header").forEach((btn) => {
+        btn.addEventListener("keydown", (e) => {
+            if ([ENTER_KEY, SPACE_KEY].includes(e.key)) {
+                e.currentTarget.querySelector('.faq-list-checkbox').click();
+            }
+        });
+    })
+});
